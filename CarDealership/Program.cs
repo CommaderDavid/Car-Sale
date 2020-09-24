dbf92b0629a7aac12fdccc7023048e3633ac04c7
David Boedigheimer <stealth259@gmail.com>
2020-09-24 14:48:34 -0700

    Add fixes to nameplaces in program and startup.

--- CarDealership/Program.cs
+++ CarDealership/Program.cs
@@ -1,12 +1,18 @@
 using Microsoft.AspNetCore.Hosting;
 using System.IO;
+using CarDealership.Models;
 
-namespace ToDoList
+namespace CarDealership
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            Car x = new Car("1974 Volkswagen Thing", 1100, 368792);
+            Car yugo = new Car("1980 Yugo Koral", 700, 56000);
+            Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
+            Car amc = new Car("1976 AMC Pacer", 400, 198000);
+
             var host = new WebHostBuilder()
               .UseKestrel()
               .UseContentRoot(Directory.GetCurrentDirectory())
